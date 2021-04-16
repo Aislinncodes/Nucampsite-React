7a705b26b4c04a3659f7dfa8191c764d3e675503
Aislinn Wallace Hendricks <aislinnwh@gmail.com>
2021-04-16 00:21:19 -0400

    Header and Footer

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
-import { CAMPSITES } from '../shared/campsites';
 
 
-class CampsiteInfo extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-    renderCampsite(campsite) {
+  function RenderCampsite({campsite}) {
       return (
         <div className="col-md-5 m-1">
           <Card>
@@ -21,7 +16,7 @@ class CampsiteInfo extends React.Component {
     );
   };
 
-    renderComments(comments) {
+  function RenderComments({comments}) {
         if(comments) {
           return(
             <div className="col-md-5 m-1">
@@ -36,20 +31,19 @@ class CampsiteInfo extends React.Component {
         return <div />;
     };
 
-  render() {
-    if (this.props.campsite) {
+  function CampsiteInfo(props) {
+    if (props.campsite) {
       return (
         <div className="container">
         <div className="row">
-          {this.renderCampsite(this.props.campsite)}
-          {this.renderComments(this.props.campsite.comments)}
+          <RenderCampsite campsite={props.campsite} />
+          <RenderComments comments={props.campsite.comments} />
         </div>
         </div>
       );
     };
     return <div />;
   };
-}
 
 export default CampsiteInfo;
 
